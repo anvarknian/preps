10ac2436d5feb97239089afc273ac8a80040ce11
anvarknian <anvarknian@yandex.ru>
2021-06-15 10:50:05 +0300

    test

---
+++ app.py
@@ -0,0 +1,24 @@
+## Merge sort, quick sort , bubble sort, insertion sort, selection sort
+## binary search,  linear search
+## rec binary search, rec linear search
+
+def merge_sort(array):
+    pass
+def quick_sort(array):
+    pass
+def bubble_sort(array):
+    pass
+def insertion_sort(array):
+    pass
+def selection_sort(array):
+    pass
+
+def binary_search(array, value):
+    pass
+def linear_search(array, value):
+    pass
+
+def rec_binary_search(array, value):
+    pass
+def rec_linear_search(array, value):
+    pass
