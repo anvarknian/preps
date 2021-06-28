42ec28ab6ce1abeedba93f25082599e1e350ebf5
anvarknian <anvarknian@yandex.ru>
2021-06-28 22:37:52 +0300

    update

---
+++ Stacks-and-Queues/stack.py
@@ -0,0 +1,13 @@
+stack = [1,2,3,4]
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
